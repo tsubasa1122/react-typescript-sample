bde32c08b856474bad725d045417c0d7e6ae170a
tsubasa1122 <nakanot@open8.com>
2020-08-22 17:33:16 +0900

    Create AddTodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
-const todos = [{ id: 't1', text: 'TypeScriptコースの完了' }];
 const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   const addTodo = (text: string) => {
-    console.log(text);
+    setTodos(prevTodos => [...prevTodos, { id: Math.random().toString(), text: text }]);
   };
+
   return <div className="App">
     <NewTodo addTodo={addTodo} />
     <TodoList items={todos} />
