375f4e65a7d862cd4f07f0e202a533b000a14807
tsubasa1122 <nakanot@open8.com>
2020-08-22 17:22:24 +0900

    Create NewTodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const todos = [{ id: 't1', text: 'TypeScriptコースの完了' }];
 const App: React.FC = () => {
   return <div className="App">
+    <NewTodo />
     <TodoList items={todos} />
   </div>;
 }
